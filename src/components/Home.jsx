4083190be70ddddb0b56e5ac7007c61896a26bed
Abdul rahib <rahibrahib6740@gmail.com>
2025-02-08 17:52:38 +0530

    home page 1st section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,9 +3,39 @@ import Navbar from "./Navbar";
 const Home = ()=> {
 
   return (
-    <div>
+    <div className=" bg-[#010a23] bg-[url(/images/home-bg.png)]">
       <Navbar />
-      <h1 className="text-white">Vite + React</h1>
+      <div className="flex">
+        <div className="flex flex-col">
+        <img className=' m-15 mx-50 w-[150px] h-[50px] lg:w-[278px] lg:h-[92px]' src="/images/cloud1.png"/>
+        <div className="flex flex-col">
+        <div className="flex relative z-0">
+        <p className="text-white font-bold lg:text-6xl sm:text-3xl flex items-center gap-2 ">
+        We Provide
+        </p>
+        <img className=" w-20 h-20 lg:w-40 lg:h-40" src="/images/bulb.png"/>
+        </div>
+        <div className="flex -mt-5 lg:-mt-10 z-10 ">
+          <div>
+          <p className="text-white  font-bold lg:text-6xl sm:text-3xl z-20">
+         Smart Business<br/>
+          Solution
+        </p>
+        <p className=" text-white mt-5">
+          Grow Your Business With Us Best Business Solution
+       </p>
+          </div>
+          <div className="lg:mt-15 lg:-mx-10">
+            <img className="w-[150px] h-[50px] lg:w-[278px] lg:h-[92px]" src="/images/cloud1.png" />
+          </div>
+        
+        </div>
+        </div>
+        </div>
+        <div>
+          <img className="lg:w-[718px] lg:h-[686px]" src="images/person.png"/>
+        </div>
+      </div>
     </div> 
   )
 }
