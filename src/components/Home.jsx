76deca479f123c73574ea4bea5a3e90d69915143
Abdul rahib <rahibrahib6740@gmail.com>
2025-02-10 11:02:32 +0530

    home 2nd commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,12 +8,13 @@ const Home = ()=> {
 
   return (
 
-    <div className=" bg-[#010a23] bg-[url(/images/home-bg.png)]">
+    <div>
+       <div className=" bg-[#010a23] bg-[url(/images/home-bg.png)]">
       <Navbar />
       <div className="flex">
         <div className="flex flex-col">
-        <img className=' m-15 mx-50 w-[150px] h-[50px] lg:w-[278px] lg:h-[92px]' src="/images/cloud1.png"/>
-        <div className="flex flex-col">
+        <img className=' mt-20 mx-30 w-[140px] h-[45px] lg:w-[258px] lg:h-[82px]' src="/images/cloud1.png"/>
+        <div className="flex flex-col mx-50">
         <div className="flex relative z-0">
         <p className="text-white font-bold lg:text-6xl sm:text-3xl flex items-center gap-2 ">
         We Provide
@@ -30,18 +31,35 @@ const Home = ()=> {
           Grow Your Business With Us Best Business Solution
        </p>
           </div>
-          <div className="lg:mt-15 lg:-mx-10">
-            <img className="w-[150px] h-[50px] lg:w-[278px] lg:h-[92px]" src="/images/cloud1.png" />
+          <div className="lg:mt-15 lg:-mx-20">
+            <img className="w-[140px] h-[45px] lg:w-[258px] lg:h-[82px]" src="/images/cloud1.png" />
           </div>
         
         </div>
         </div>
         </div>
-        <div>
-          <img className="lg:w-[718px] lg:h-[686px]" src="images/person.png"/>
+        <div className='flex flex-col mt-10 -mx-80'>
+            <img className=" mx-80 w-[140px] h-[45px] lg:w-[258px] lg:h-[82px]" src="/images/cloud1.png" />
+            <div className='flex'>
+            <img className=" -mt-10 mx-20 w-[260px] h-[210px]  md:w-[360] md:h-[310px] lg:w-[718px] lg:h-[686px]" src="images/person.png"/>
+            <img className='mt-30 -mx-30 w-[30px] h-[30px] lg:w-[70px] lg:h-[70px]' src="/images/Ball.png"/>
+            </div>
         </div>
       </div>
     </div> 
+    <div className='bg-[url(/images/vector-1.png)]'>
+      <div className='flex flex-col items-center'>
+        <div>
+          <img src="/images/curlLine.png"/>
+          <p class="font-extrabold text-[80px] sm:text-[50px] md:text-[70px] lg:text-[100px] leading-[150px] tracking-tight uppercase text-transparent stroke-white">
+             BRAND
+        </p>
+
+        </div>
+      </div>
+    </div>
+    </div>
+   
 
   )
 }
