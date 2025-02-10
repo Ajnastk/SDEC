5767754f914567f3feecfc0dee71e54c2ea1f094
muhammed ajnas tk <ajnastk432@gmail.com>
2025-02-10 15:57:16 +0530

    who section 1st

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Home from "./components/Home";
 import Feature from "./components/Feature";
+import Who from "./components/Who"
 
 const App=()=>{
     return(
         <div className="bg-[#000A1F]">
             <Home/>
             <Feature/>
+            <Who/>
         </div>
     )
 }
