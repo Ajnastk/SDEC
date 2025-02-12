0a6f707672eb5954009acdb8565f88ef5c36ca13
ziyad <ziyadmuhammad9696@gmail.com>
2025-02-12 16:50:01 +0530

    temparery

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,25 @@
 import Home from "./components/Home";
 import About from "./components/About";
 import Seo from "./components/Seo";
-import Services from './components/Services'
-import Projects from './components/Projects';
+import Services from "./components/Services";
+import Projects from "./components/Projects";
 import Call from "./components/Call";
 import Says from "./components/Says";
 
-
-
 const App = () => {
-    return (
-
-
-      <div className="bg-[#000A1F] min-h-screen w-[1920px] overflow-hidden">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
-
-          <Home />
-          <About />
-          <Services />
-          <Projects />
-          <Seo/>
-          <Says/>
-          <Call/>
-        </div>
-    );
-  };
-  
+  return (
+    <div className="bg-[#000A1F] min-h-screen w-[1920px] overflow-hidden">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
+        <Home />
+        <About />
+        <Services />
+        <Projects />
+        <Seo />
+        <Says />
+        <Call />
+      </div>
+    </div>
+  );
+};
 
 export default App;
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,7 +8,7 @@ import 'swiper/css/pagination';
 
 const Projects=()=>{
 
-    const projectsData = contents.Projects;
+    const projectsData = contents.ourProjects;
     return(
         <div className="flex flex-col  py-20 text-white items-center">
             <div className="flex flex-col items-center">
@@ -34,8 +34,7 @@ const Projects=()=>{
                 768:{slidesPerView:2},
                 1024:{slidesPerView:3,slidesPerGroup:1},
               }}>
-                {projectsData
-                .ourProjects.map((project)=>(
+                {projectsData.ourProjects.map((project)=>(
                     <SwiperSlide key={project.id} className="flex justify-center">
                         <div className="bg-gray-900 p-4 rounded-xl shadow-lg relative w-full mx-w-xs">
                             <img src={project.image}
