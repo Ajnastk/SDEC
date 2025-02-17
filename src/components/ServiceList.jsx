31a83367a91f119f9bb36f0ce9b11bec9de1709e
ziyad <ziyadmuhammad9696@gmail.com>
2025-02-17 14:52:24 +0530

    animation added service component

--- src/components/ServiceList.jsx
+++ src/components/ServiceList.jsx
@@ -1,14 +1,39 @@
 import { contents } from "../contents";
+import { motion } from "framer-motion"; // Import framer-motion for animations
+import { useState } from "react";
 
 const ServiceList = () => {
   const { services } = contents;
+  const [isInView, setIsInView] = useState(false);
+
+  const handleInView = (inView) => {
+    setIsInView(inView);
+  };
+  // Animation variants for the service items
+  const itemVariants = {
+    hidden: { opacity: 0, x: -100 }, // Start from the left side
+    visible: { opacity: 1, x: 0 }, // Move to the original position (right side)
+  };
+
   return (
     <div>
       {/* Show only Desktop View */}
       <div className="space-y-6 ml-[20%] hidden lg:block">
         {/* graphicsDesign */}
-        <div className="ml-[160px]">
-          <div className="flex items-center w-[90%] md:w-[400px] p-5 bg-[rgba(41,39,56,1)] rounded-lg shadow-lg transition-transform transform hover:scale-101">
+        <motion.div
+          className="ml-[160px]"
+          initial="hidden"
+          whileInView="visible"
+          animate={isInView ? "visible" : "hidden"}  // Reverts animation to "hidden" when not in view
+          onViewportBoxUpdate={(_, info) => handleInView(info.isIntersecting)} // Update state based on visibility
+          // viewport={{ once: true }}
+          variants={itemVariants}
+          transition={{
+            duration: 0.8, // Increase duration for smoother animation
+            ease: "easeOut", // Smoother easing
+          }}
+        >
+          <div className="flex items-center w-[90%] md:w-[400px] p-5 bg-[rgba(41,39,56,1)] rounded-lg shadow-lg transition-transform transform hover:scale-102">
             {/* Service Icon */}
             <div className="w-16 flex items-center justify-center">
               <img
@@ -33,9 +58,22 @@ const ServiceList = () => {
               ➜
             </div>
           </div>
-        </div>
+        </motion.div>
+
         {/* ideationAndEvaluation */}
-        <div className="ml-[210px]">
+        <motion.div
+          className="ml-[210px]"
+          initial="hidden"
+          whileInView="visible"
+          animate={isInView ? "visible" : "hidden"}  // Reverts animation to "hidden" when not in view
+          onViewportBoxUpdate={(_, info) => handleInView(info.isIntersecting)} // Update state based on visibility
+          // viewport={{ once: true }}
+          variants={itemVariants}
+          transition={{
+            duration: 0.8, // Increase duration for smoother animation
+            ease: "easeOut", // Smoother easing
+          }}
+        >
           <div className="flex items-center w-[90%] md:w-[400px] p-5 bg-[rgba(41,39,56,1)] rounded-lg shadow-lg transition-transform transform hover:scale-101">
             {/* Service Icon */}
             <div className="w-16 flex items-center justify-center">
@@ -61,9 +99,22 @@ const ServiceList = () => {
               ➜
             </div>
           </div>
-        </div>
+        </motion.div>
+
         {/* brandIdentity */}
-        <div className="ml-[260px]">
+        <motion.div
+          className="ml-[260px]"
+          initial="hidden"
+          whileInView="visible"
+          animate={isInView ? "visible" : "hidden"}  // Reverts animation to "hidden" when not in view
+          onViewportBoxUpdate={(_, info) => handleInView(info.isIntersecting)} // Update state based on visibility
+          // viewport={{ once: true }}
+          variants={itemVariants}
+          transition={{
+            duration: 0.8, // Increase duration for smoother animation
+            ease: "easeOut", // Smoother easing
+          }}
+        >
           <div className="flex items-center w-[90%] md:w-[400px] p-5 bg-[rgba(41,39,56,1)] rounded-lg shadow-lg transition-transform transform hover:scale-101">
             {/* Service Icon */}
             <div className="w-16 flex items-center justify-center">
@@ -89,9 +140,22 @@ const ServiceList = () => {
               ➜
             </div>
           </div>
-        </div>
+        </motion.div>
+
         {/* customerService */}
-        <div className="ml-[140px]">
+        <motion.div
+          className="ml-[140px]"
+          initial="hidden"
+          whileInView="visible"
+          animate={isInView ? "visible" : "hidden"}  // Reverts animation to "hidden" when not in view
+          onViewportBoxUpdate={(_, info) => handleInView(info.isIntersecting)} // Update state based on visibility
+          // viewport={{ once: true }}
+          variants={itemVariants}
+          transition={{
+            duration: 0.8, // Increase duration for smoother animation
+            ease: "easeOut", // Smoother easing
+          }}
+        >
           <div className="flex items-center w-[90%] md:w-[400px] p-5 bg-[rgba(41,39,56,1)] rounded-lg shadow-lg transition-transform transform hover:scale-101">
             {/* Service Icon */}
             <div className="w-16 flex items-center justify-center">
@@ -117,9 +181,22 @@ const ServiceList = () => {
               ➜
             </div>
           </div>
-        </div>
+        </motion.div>
+
         {/* webStrategy */}
-        <div className="mr-[90%]">
+        <motion.div
+          className="mr-[90%]"
+          initial="hidden"
+          whileInView="visible"
+          animate={isInView ? "visible" : "hidden"}  // Reverts animation to "hidden" when not in view
+          onViewportBoxUpdate={(_, info) => handleInView(info.isIntersecting)} // Update state based on visibility
+          // viewport={{ once: true }}
+          variants={itemVariants}
+          transition={{
+            duration: 0.8, // Increase duration for smoother animation
+            ease: "easeOut", // Smoother easing
+          }}
+        >
           <div className="flex items-center w-[90%] md:w-[400px] p-5 bg-[rgba(41,39,56,1)] rounded-lg shadow-lg transition-transform transform hover:scale-101">
             {/* Service Icon */}
             <div className="w-16 flex items-center justify-center">
@@ -145,14 +222,27 @@ const ServiceList = () => {
               ➜
             </div>
           </div>
-        </div>
+        </motion.div>
       </div>
+
       {/* Show only Mobile view */}
       <div className="w-full flex flex-col items-center justify-center lg:hidden px-4 sm:px-6 space-y-4">
         {Object.keys(services.servicesList).map((key, index) => {
           const service = services.servicesList[key];
           return (
-            <div key={index} className="w-full max-w-[400px]">
+            <motion.div
+              key={index}
+              className="w-full max-w-[400px]"
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true }}
+              variants={itemVariants}
+              transition={{
+                duration: 0.8, // Increase duration for smoother animation
+                ease: "easeOut", // Smoother easing
+                delay: index * 0.3, // Delay for a staggered effect
+              }}
+            >
               <div className="flex items-center p-5 bg-[rgba(41,39,56,1)] rounded-lg shadow-lg transition-transform transform hover:scale-105">
                 {/* Service Icon */}
                 <div className="w-16 flex items-center justify-center">
@@ -174,7 +264,7 @@ const ServiceList = () => {
                   ➜
                 </div>
               </div>
-            </div>
+            </motion.div>
           );
         })}
       </div>
