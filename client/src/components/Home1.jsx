6cfea3d48fbdd0a474eff44044b759f98ab8fbf4
muhammed ajnas tk <ajnastk432@gmail.com>
2025-02-18 18:15:21 +0530

    brand

--- client/src/components/Home1.jsx
+++ client/src/components/Home1.jsx
@@ -17,7 +17,7 @@ import object4Png from "../assets/images/bulb.png";
 import pseudo from "../assets/images/headingLineImage.png";
 import vector1 from "../assets/images/Vector.svg";
 import Menu from "../assets/images/menu.svg";
-import Stroke from "../assets/images/grid.svg";
+import Stroke from "../assets/images/Grid.svg";
 
  const Home1 = () => {
   return (
@@ -26,6 +26,57 @@ import Stroke from "../assets/images/grid.svg";
     >
       <div className="relative h-[1410px]">
         <div className="absolute  h-[904px] top-0 left-0">
+        <div className="absolute w-[1111px] h-[49px] top-16 left-[310px]">
+         
+         <div className="absolute w-[1111px] h-[49px] top-16 left-[405px]">SDEC</div>
+
+         <div className="absolute w-[682px] h-[30px] top-0 left-[100px]">
+           <div className="absolute w-[70px] h-[30px] top-0 left-0">
+             <div className="absolute w-[53px] h-[30px] top-0 left-0">
+               <div className="absolute w-[53px] h-px top-[27px] left-0 bg-white" />
+               <div className="absolute h-[30px] top-0 left-0 [font-family:'Jost-Medium',Helvetica] font-medium text-white text-xl text-center tracking-[0] leading-[30px] whitespace-nowrap">
+                 Home
+               </div>
+             </div>
+             <div className="absolute h-6 top-px left-[58px] [font-family:'Font_Awesome_5_Free-Solid',Helvetica] font-normal text-white text-xs text-center tracking-[0] leading-6 whitespace-nowrap">
+               
+             </div>
+           </div>
+           <div className="h-[30px] top-0 left-[109px] [font-family:'Jost-Medium',Helvetica] font-medium text-xl leading-[30px] absolute text-white text-center tracking-[0] whitespace-nowrap">
+             Clients
+           </div>
+           <div className="h-6 top-px left-[173px] [font-family:'Font_Awesome_5_Free-Solid',Helvetica] font-normal text-xs leading-6 absolute text-white text-center tracking-[0] whitespace-nowrap">
+             
+           </div>
+           <div className="h-[30px] top-0 left-[225px] [font-family:'Jost-Medium',Helvetica] font-medium text-xl leading-[30px] absolute text-white text-center tracking-[0] whitespace-nowrap">
+             Services
+           </div>
+           <div className="left-[304px] absolute h-6 top-px [font-family:'Font_Awesome_5_Free-Solid',Helvetica] font-normal text-white text-xs text-center tracking-[0] leading-6 whitespace-nowrap">
+             
+           </div>
+           <div className="h-[30px] top-0 left-[358px] [font-family:'Jost-Medium',Helvetica] font-medium text-xl leading-[30px] absolute text-white text-center tracking-[0] whitespace-nowrap">
+             Projects
+           </div>
+           <div className="left-[433px] absolute h-6 top-px [font-family:'Font_Awesome_5_Free-Solid',Helvetica] font-normal text-white text-xs text-center tracking-[0] leading-6 whitespace-nowrap">
+             
+           </div>
+           <div className="h-[30px] top-0 left-[485px] [font-family:'Jost-Medium',Helvetica] font-medium text-xl leading-[30px] absolute text-white text-center tracking-[0] whitespace-nowrap">
+             About
+           </div>
+           <div className="left-[543px] absolute h-6 top-px [font-family:'Font_Awesome_5_Free-Solid',Helvetica] font-normal text-white text-xs text-center tracking-[0] leading-6 whitespace-nowrap">
+             
+           </div>
+           <div className="h-[30px] top-0 left-[597px] [font-family:'Jost-Medium',Helvetica] font-medium text-xl leading-[30px] absolute text-white text-center tracking-[0] whitespace-nowrap">
+             Contact
+           </div>
+         </div>
+         <img
+           className="absolute w-7 h-[22px] top-0.5 left-[1083px]"
+           alt="Vector"
+           src={Menu}
+         />
+       </div>
+     </div>
           <div className="relative h-[904px]">
          
             <img
@@ -36,18 +87,84 @@ import Stroke from "../assets/images/grid.svg";
           
           </div>
         </div>
-        <img
-          className="absolute h-[740px] top-[670px] left-0.5"
+       
+        <div className="absolute w-[1422px] h-[490px] top-[123px] left-[238px]">
+          <img
+            className="absolute w-[278px] h-[277px] top-0 left-[900px]"
+            alt="Image"
+            src={cloud3}
+          />
+          <img
+            className="absolute w-[278px] h-[159px] top-[331px] left-[483px]"
+            alt="Image"
+            src={cloud2}
+          />
+          <img
+            className="absolute w-[278px] h-[92px] top-[47px] right-[1300px]"
+            alt="Image"
+            src={cloud1}
+          />
+        </div>
+        <div className="absolute w-[780px] h-[686px] top-[161px] left-[720px]">
+          <div className="relative h-[686px]">
+            <img
+              className="absolute w-[718px] h-[668px] top-0 "
+              alt="Image"
+              src={image4}
+            />
+            <img
+              className="absolute w-[107px] h-[107px] top-[179px] left-[673px]"
+              alt="Image"
+              src={image5}
+            />
+          </div>
+        </div>
+        <div className="absolute w-[720px] h-[225px] top-[281px] left-[405px]">
+          <div className="absolute w-[548px] h-[310px] top-[-70px] left-0">
+            <img
+              className="absolute w-[206px] h-[214px] -top-[20px] left-[190px] "
+              alt="Object png"
+              src={object4Png}
+            />
+            <div className="absolute w-[550px] h-[258px] top-[52px] right-[150px] [font-family:'Jost-SemiBold',Helvetica] font-semibold text-white text-[75px] tracking-[-0.75px] leading-[75px]">
+              We Provide<br/>
+              smart Business<br />
+              solutions
+            </div>
+          </div>
+          <p className="absolute w-[441px] h-[29px] top-[248px] right-[425px] [font-family:'Jost-Regular',Helvetica] font-normal text-white text-xl tracking-[0] leading-[30px] whitespace-nowrap">
+            Grow your Business With Us Best Business Solutions
+          </p>
+        </div>
+       
+       <img
+          className="absolute  h-[777px] top-[756px] "
           alt="Vector"
           src={vector1}
         />
-        <div className="absolute bg-amber-400 w-[1000px] h-[295px] top-[1084px] left-[375px]">
+        <div className="absolute w-[1000px] h-[295px] top-[1084px] left-[155px]">
+        <div className="absolute w-[559px] h-[197px] top-[-61px] left-[30px]">
+            <div className="relative w-[555px] h-[198px] -top-px -left-px">
+              <img
+                className="absolute w-[84px] h-2.5 top-[60px] left-[41px]"
+                alt="Pseudo"
+                src={pseudo}
+              />
+              <div className="absolute w-[555px] h-[186px] top-0 left-0 [-webkit-text-stroke:1px_#ffffff26] [font-family:'Kumbh_Sans-Black',Helvetica] font-black text-transparent text-[150px] tracking-[0] leading-7">
+                BRAND
+              </div>
+              <p className="absolute w-[466px] h-[114px] top-[84px] left-[41px] [font-family:'Jost-SemiBold',Helvetica] font-semibold text-white text-[42px] tracking-[0.42px] leading-[53px]">
+                We’ve More Then 254+ <br />
+                Global Partners
+              </p>
+            </div>
+          </div>
           <p className="absolute w-[463px] h-[84px] top-[26px] left-[677px] [font-family:'Kumbh_Sans-Regular',Helvetica] font-normal text-[#a8adb3] text-base tracking-[0] leading-8">
             Sed ut perspiciatis unde omnis natus error voluptatem santium <br />
             doloremque laudantium, totam rem aperiam, eaque ipsa quae <br />
             ab nllo inventore veritatis quasi architecto beatae vitae
           </p>
-          <div className="absolute w-[1147px] h-24 top-[196px] left-3">
+          <div className="absolute min-w-s h-24 top-[196px] left-3">
             <div className="absolute w-[1003px] h-20 top-4 left-[72px]">
               <img
                 className="absolute w-[155px] h-20 top-0 left-[210px]"
@@ -91,122 +208,7 @@ import Stroke from "../assets/images/grid.svg";
               src={button1}
             />
           </div>
-          <div className="absolute w-[559px] h-[197px] top-[-61px] left-[30px]">
-            <div className="relative w-[555px] h-[198px] -top-px -left-px">
-              <img
-                className="absolute w-[84px] h-2.5 top-[60px] left-[41px]"
-                alt="Pseudo"
-                src={pseudo}
-              />
-              <div className="absolute w-[555px] h-[186px] top-0 left-0 [-webkit-text-stroke:1px_#ffffff26] [font-family:'Kumbh_Sans-Black',Helvetica] font-black text-transparent text-[150px] tracking-[0] leading-7">
-                BRAND
-              </div>
-              <p className="absolute w-[466px] h-[114px] top-[84px] left-[41px] [font-family:'Jost-SemiBold',Helvetica] font-semibold text-white text-[42px] tracking-[0.42px] leading-[53px]">
-                We’ve More Then 254+ <br />
-                Global Partners
-              </p>
-            </div>
-          </div>
         </div>
-        <div className="absolute w-[1422px] h-[490px] top-[123px] left-[238px]">
-          <img
-            className="absolute w-[278px] h-[277px] top-0 left-[1144px]"
-            alt="Image"
-            src={cloud3}
-          />
-          <img
-            className="absolute w-[278px] h-[159px] top-[331px] left-[583px]"
-            alt="Image"
-            src={cloud2}
-          />
-          <img
-            className="absolute w-[278px] h-[92px] top-[47px] left-0"
-            alt="Image"
-            src={cloud1}
-          />
-        </div>
-        <div className="absolute w-[780px] h-[686px] top-[161px] left-[982px]">
-          <div className="relative h-[686px]">
-            <img
-              className="absolute w-[718px] h-[686px] top-0 left-0"
-              alt="Image"
-              src={image4}
-            />
-            <img
-              className="absolute w-[107px] h-[107px] top-[179px] left-[673px]"
-              alt="Image"
-              src={image5}
-            />
-          </div>
-        </div>
-        <div className="absolute w-[720px] h-[225px] top-[281px] left-[405px]">
-          <div className="absolute w-[548px] h-[310px] top-[-70px] left-0">
-            <img
-              className="absolute w-[206px] h-[214px] -top-[20px] left-[342px] "
-              alt="Object png"
-              src={object4Png}
-            />
-            <div className="absolute w-[550px] h-[258px] top-[52px] left-0 [font-family:'Jost-SemiBold',Helvetica] font-semibold text-white text-[75px] tracking-[-0.75px] leading-[75px]">
-              We Provide<br/>
-              smart Business<br />
-              solutions
-            </div>
-          </div>
-          <p className="absolute w-[441px] h-[29px] top-[248px] left-0 [font-family:'Jost-Regular',Helvetica] font-normal text-white text-xl tracking-[0] leading-[30px] whitespace-nowrap">
-            Grow your Business With Us Best Business Solutions
-          </p>
-        </div>
-        <div className="absolute w-[1111px] h-[49px] top-16 left-[405px]">
-         
-          <div className="absolute w-[1111px] h-[49px] top-16 left-[405px]">SDEC</div>
-
-          <div className="absolute w-[682px] h-[30px] top-0 left-[272px]">
-            <div className="absolute w-[70px] h-[30px] top-0 left-0">
-              <div className="absolute w-[53px] h-[30px] top-0 left-0">
-                <div className="absolute w-[53px] h-px top-[27px] left-0 bg-white" />
-                <div className="absolute h-[30px] top-0 left-0 [font-family:'Jost-Medium',Helvetica] font-medium text-white text-xl text-center tracking-[0] leading-[30px] whitespace-nowrap">
-                  Home
-                </div>
-              </div>
-              <div className="absolute h-6 top-px left-[58px] [font-family:'Font_Awesome_5_Free-Solid',Helvetica] font-normal text-white text-xs text-center tracking-[0] leading-6 whitespace-nowrap">
-                
-              </div>
-            </div>
-            <div className="h-[30px] top-0 left-[109px] [font-family:'Jost-Medium',Helvetica] font-medium text-xl leading-[30px] absolute text-white text-center tracking-[0] whitespace-nowrap">
-              Clients
-            </div>
-            <div className="h-6 top-px left-[173px] [font-family:'Font_Awesome_5_Free-Solid',Helvetica] font-normal text-xs leading-6 absolute text-white text-center tracking-[0] whitespace-nowrap">
-              
-            </div>
-            <div className="h-[30px] top-0 left-[225px] [font-family:'Jost-Medium',Helvetica] font-medium text-xl leading-[30px] absolute text-white text-center tracking-[0] whitespace-nowrap">
-              Services
-            </div>
-            <div className="left-[304px] absolute h-6 top-px [font-family:'Font_Awesome_5_Free-Solid',Helvetica] font-normal text-white text-xs text-center tracking-[0] leading-6 whitespace-nowrap">
-              
-            </div>
-            <div className="h-[30px] top-0 left-[358px] [font-family:'Jost-Medium',Helvetica] font-medium text-xl leading-[30px] absolute text-white text-center tracking-[0] whitespace-nowrap">
-              Projects
-            </div>
-            <div className="left-[433px] absolute h-6 top-px [font-family:'Font_Awesome_5_Free-Solid',Helvetica] font-normal text-white text-xs text-center tracking-[0] leading-6 whitespace-nowrap">
-              
-            </div>
-            <div className="h-[30px] top-0 left-[485px] [font-family:'Jost-Medium',Helvetica] font-medium text-xl leading-[30px] absolute text-white text-center tracking-[0] whitespace-nowrap">
-              About
-            </div>
-            <div className="left-[543px] absolute h-6 top-px [font-family:'Font_Awesome_5_Free-Solid',Helvetica] font-normal text-white text-xs text-center tracking-[0] leading-6 whitespace-nowrap">
-              
-            </div>
-            <div className="h-[30px] top-0 left-[597px] [font-family:'Jost-Medium',Helvetica] font-medium text-xl leading-[30px] absolute text-white text-center tracking-[0] whitespace-nowrap">
-              Contact
-            </div>
-          </div>
-          <img
-            className="absolute w-7 h-[22px] top-0.5 left-[1083px]"
-            alt="Vector"
-            src={Menu}
-          />
-        </div>
-      </div>
     </div>
   );
 };
