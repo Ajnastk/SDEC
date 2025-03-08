4cdf7f5a70c467802e9418ffeb7ca2c426bbda69
muhammed ajnas tk <ajnastk432@gmail.com>
2025-03-08 17:00:02 +0530

    mount

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -24,28 +24,15 @@ const Home = () => {
     >
       <Navbar />
       <div className=" flex flex-col lg:flex-row justify-center px-5 lg:px-20 ">
-        <div className=" items-center lg:items-start lg:mx-20">
+        <div className=" items-center lg:items-start lg:mx-15">
           <img
             className="mt-5 w-[120px] h-[40px] sm:w-[150px] sm:h-[50px] lg:w-[278px] lg:h-[92px]"
             src={cloud1}
             alt="Cloud"
           />
-          <div>
-            <div className="flex">
+            <div>
               <FlipWords />
             </div>
-
-            {/* <div className="flex bg-amber-900 items-center">
-              <p className="text-white font-bold text-3xl lg:text-6xl  w-[160px] sm:w-[190px] md:w-[200px] lg:w-[400px] sm:text-4xl">
-                We Provide
-              </p>
-              <img
-                className="relative lg:right-[100px] w-16 h-16 sm:w-20 sm:h-20 lg:w-40 lg:h-40"
-                src={bulb}
-                alt="Bulb"
-              />
-            </div> */}
-          </div>
         </div>
 
         <div className="flex flex-col items-center mt-5 lg:mt-0 lg:-mx-30 z-51">
@@ -54,9 +41,9 @@ const Home = () => {
             src={cloud1}
             alt="Cloud"
           />
-          <div className="flex items-center justify-center gap-5 sm:gap-10">
+          <div className="flex items-center justify-center sm:gap-10">
             <img
-              className="w-[400px] sm:w-[400px] md:w-[500px] lg:w-[600px] z-30"
+              className="w-[400px] sm:w-[400px] md:w-[450px] lg:w-[550px] z-30"
               src={person}
               alt="Person"
             />
@@ -69,14 +56,17 @@ const Home = () => {
         </div>
       </div>
       <div
-        className="absolute md:bottom-100 bottom-[-20px] lg:bottom-0 left-0 sm:w-full md:w-full lg:w-full w-full h-1/3 z-20"
-        style={{
-          backgroundImage: `url(${vector})`,
-          backgroundSize: "cover",
-          backgroundPosition: "top center",
-          backgroundRepeat: "no-repeat",
-        }}
-      ></div>
+  className="absolute left-0 w-full z-20
+    h-[25vh] sm:h-[30vh] md:h-[35vh] lg:h-[40vh]
+    bottom-[-10px] sm:bottom-[-15px] md:bottom-[-20px] lg:bottom-0
+    transform scale-y-[1.5] sm:scale-y-[1.1] md:scale-y-[1] lg:scale-y-[1]"
+  style={{
+    backgroundImage: `url(${vector})`,
+    backgroundSize: "100% 100%",
+    backgroundPosition: "top center",
+    backgroundRepeat: "no-repeat",
+  }}
+></div>
       {/* <motion.div
           variants={fadeIn}
           initial="hidden"
